f84c5b5776a4c06730db7d335e89fe5b908b55f9
Teodor B <teomati@gmail.com>
2018-06-08 23:50:14 +0200

    nowyouseethistoo3

--- main.go
+++ main.go
@@ -53,7 +53,7 @@ func main(){
 				container = "rtorrent"
 			case "teo-mateo/webfish":
 				fmt.Println("Adding timestamp for WEBFISH repo")
-				fmt.Println("Now you see this too")
+				fmt.Println("Now you see this too00000")
 				container = "webfish"
 			default:
 				fmt.Println("Unknown repo: " + payload.Repository.FullName)
