c0e1bb170e89dc35768c2c947daea7082f2a311d
Teodor B <teomati@gmail.com>
2018-06-08 21:04:37 +0200

    webfish impl

--- main.go
+++ main.go
@@ -7,14 +7,24 @@ import (
 	"strconv"
 	"io/ioutil"
 	"github.com/davecgh/go-spew/spew"
+	"os"
+	"encoding/json"
+	"time"
 )
 
 func main(){
 	fmt.Println("web fish")
 
 	var port int
+	var file string
 	flag.IntVar(&port, "port", 80, "--port=80")
+	flag.StringVar(&file, "file", "", "--file=<path/to/file>")
 	flag.Parse()
+
+	if file == ""{
+		fmt.Println("missing arg: file")
+		os.Exit(1)
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
 
@@ -27,11 +37,57 @@ func main(){
 			fmt.Println(string(body))
 		}
 
-		w.Write([]byte("Yes webfish"))
+		payload := &GitHubPayload{}
+		err = json.Unmarshal(body, payload)
+
+		if err != nil{
+			fmt.Println(err)
+		}
+
+		container := ""
+
+		switch payload.Repository.FullName {
+			case "teo-mateo/flbrowser":
+				fmt.Println("Adding timestamp for FLBROWSER repo")
+				container = "rtorrent"
+			default:
+				fmt.Println("Unknown repo: " + payload.Repository.FullName)
+		}
+
+		if container != ""{
+			err = appendContainerToFile(file, container)
+			if err != nil{
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
 	})
 	var host = "0.0.0.0:"+strconv.Itoa(port)
 	fmt.Println("Listening at " + host)
 
 	http.ListenAndServe(host, nil)
 
+}
+
+func appendContainerToFile(file string, repo string) error {
+	now := time.Now()
+	s := fmt.Sprintf("%s|%s", now.Format("20060102030405"), repo)
+
+	f, err := os.OpenFile(file, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+	if err != nil{
+		return err
+	}
+
+	defer f.Close()
+
+	f.WriteString(s)
+	return nil
+}
+
+type GitHubPayload struct{
+	Ref string `json:"ref"`
+	Repository struct {
+		Name string `json:"name"`
+		FullName string `json:"full_name"`
+	} `json:"repository"`
 }
